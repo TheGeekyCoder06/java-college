bdeff103f151ef877e019dd716ce763d43f0246e
TheGeekyCoder06 <mharshith200@gmail.com>
2025-11-29 11:13:21 +0530

    29 nov files add

---
+++ Main.java
@@ -0,0 +1,49 @@
+class WrongAgeException extends Exception {
+    public WrongAgeException(String message) {
+        super(message);
+    }
+}
+
+class SonAgeException extends Exception {
+    public SonAgeException(String message) {
+        super(message);
+    }
+}
+
+class Father {
+    int fatherAge;
+
+    Father(int age) throws WrongAgeException {
+        if (age < 0) {
+            throw new WrongAgeException("Father's age cannot be negative");
+        }
+        this.fatherAge = age;
+        System.out.println("Father's age set to " + fatherAge);
+    }
+}
+
+class Son extends Father {
+    int sonAge;
+
+    Son(int fatherAge, int sonAge) throws WrongAgeException, SonAgeException {
+        super(fatherAge);
+
+        if (sonAge >= fatherAge) {
+            throw new SonAgeException("Son's age cannot be >= Father's age");
+        }
+        this.sonAge = sonAge;
+        System.out.println("Son's age set to " + sonAge);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Son s = new Son(-1, -1);
+        } catch (WrongAgeException e) {
+            System.out.println("Wrong Age exception: " + e.getMessage());
+        } catch (SonAgeException e) {
+            System.out.println("Son age error: " + e.getMessage());
+        }
+    }
+}
